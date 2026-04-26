bc23db5e3bf7c7570688cfdb3251046384fefced
rayan <jeffishim43@protonmail.com>
2026-04-26 11:44:40 +0100

    Made autobuild.bat work for compressor

--- autobuild.bat
+++ autobuild.bat
@@ -4,19 +4,27 @@ setlocal enabledelayedexpansion
 :: 1. Make sure we are in the right place and bin exists
 if not exist "bin" mkdir "bin"
 
-:: 2. Loop through every .cpp file in the src folder
-:: Note: In a .bat file, you MUST use double %% for variables
+:: 2. Build video-compressor separately (multi-file + FFmpeg)
+echo ---------------------------------
+echo Compiling: video-compressor...
+g++ -std=c++17 src/compress.cpp src/main.cpp -I C:/ffmpeg/include -I src/include -L C:/ffmpeg/lib -lavcodec -lavformat -lavutil -lavdevice -lswscale -lswresample -lpthread -o bin/compressor.exe
+if !errorlevel! equ 0 (
+    echo [SUCCESS] Created bin\compressor.exe
+) else (
+    echo [ERROR] Failed to compile video-compressor
+)
+
+:: 3. Loop through single-file .cpp files (skip compress.cpp and main.cpp)
 for %%f in (src\*.cpp) do (
-    echo ---------------------------------
-    echo Compiling: %%~nf...
-    
-    :: The command uses the full path to the file and outputs to bin
-    g++ "%%f" -I src/include -o "bin\%%~nf.exe"
-    
-    if !errorlevel! equ 0 (
-        echo [SUCCESS] Created bin\%%~nf.exe
-    ) else (
-        echo [ERROR] Failed to compile %%~nf
+    if /i not "%%~nf"=="compress" if /i not "%%~nf"=="main" (
+        echo ---------------------------------
+        echo Compiling: %%~nf...
+        g++ "%%f" -I src/include -o "bin\%%~nf.exe"
+        if !errorlevel! equ 0 (
+            echo [SUCCESS] Created bin\%%~nf.exe
+        ) else (
+            echo [ERROR] Failed to compile %%~nf
+        )
     )
 )
 
